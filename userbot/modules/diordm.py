c3e8ba213cfa7366f535cc7460e03501fd553de0
fatur tapi dior <90284737+DIORrios285@users.noreply.github.com>
2022-02-07 10:23:57 +0800

    Update diordm.py

--- userbot/modules/diordm.py
+++ userbot/modules/diordm.py
@@ -22,16 +22,16 @@ async def remoteaccess(event):
     mssg = await event.get_reply_message()
     if event.reply_to_msg_id:
         await event.client.send_message(chat_id, mssg)
-        await event.edit("`Success Mengirim Pesan Anda.`")
+        await event.edit("⚜️**Successfully sent your message**⚜️")
     for i in m[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
         await event.client.send_message(chat_id, msg)
-        await event.edit("`Success Mengirim Pesan Anda.`")
+        await event.edit("⚜️**Successfully sent your message**⚜️")
     except BaseException:
-        await event.edit("**Terjadi Error. Lu limit kayanya tot.**")
+        await event.edit("**Terjadi Error. Lu limit kayanya nyet.**")
 
 CMD_HELP.update(
     {
