7a25ac49b1e00472bfe8bf1f39ee83f422f3bcc4
dior <90284737+DIORrios285@users.noreply.github.com>
2022-02-20 15:06:47 +0800

    Update funtions.py

--- userbot/utils/funtions.py
+++ userbot/utils/funtions.py
@@ -33,10 +33,12 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
                                                    stdout=asyncio.subprocess.PIPE,
                                                    stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await process.communicate()
-    return (stdout.decode('utf-8', 'replace').strip(),
-            stderr.decode('utf-8', 'replace').strip(),
-            process.returncode,
-            process.pid)
+    return (
+        stdout.decode('utf-8', 'replace').strip(),
+        stderr.decode('utf-8', 'replace').strip(),
+        process.returncode,
+        process.pid,
+    )
 
 
 async def ytsearch(query, limit):
